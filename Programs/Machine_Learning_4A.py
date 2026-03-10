23f70e4a415e70aadae7244e4a0f3f76bc02f099
Enzo CHADEVILLE <chadevilleenzo@gmail.com>
2026-03-10 15:39:08 +0100

    regression linéaire OK

--- Programs/Machine_Learning_4A.py
+++ Programs/Machine_Learning_4A.py
@@ -26,7 +26,7 @@ def ComputeSimpleLinRegresCoefAnalytical(x, y):
     a1 = (n * sum_xy - sum_x * sum_y) / (n * sum_x_squared - sum_x**2)
     a0 = (sum_y - a1 * sum_x) / n
 
-    return a1, a0
+    return a0, a1
 
 # SOUS FORME DE MATRICE, on peut aussi calculer les coefficients a0 et a1 de la regression linéaire simple
 # on cherche a0 et a1 de la forme (a1, a0) = (X^T * X)^(-1) * X^T * Y)
@@ -47,10 +47,37 @@ def ComputeSimpleLinRegresCoefMatrix(x, y):
 
     # Calcul des coefficients a1 et a0
     coefficients = np.linalg.inv(X.T @ X) @ X.T @ Y
-    print("Coefficients (a1, a0) :\n", coefficients)
+    print("Coefficients (a0, a1) :\n", coefficients)
 
-    a1, a0 = coefficients.flatten()
-    return a1, a0
+    a0, a1 = coefficients.flatten()
+    return a0, a1
+
+def ComputeSimpleLinRegresCoefGradientDescent(x, y, learning_rate=0.01, max_iterations=1000, tolerance = 1e-6):
+    n = len(x)
+    a0, a1 = 0.0, 0.0
+    for iteration in range(max_iterations):
+        y_pred = predict(a1, a0, x)
+        error = y_pred - y
+        
+        # Calcul des gradients
+        da1 = (2/n) * np.sum(error * x)
+        da0 = (2/n) * np.sum(error)
+
+        if iteration > 0 and (abs(da1 - da1_prev) < tolerance and abs(da0 - da0_prev) < tolerance):
+            print(f"Convergence atteinte après {iteration} itérations.")
+            break
+
+        da1_prev = da1
+        da0_prev = da0
+
+        # Mise à jour des coefficients
+        a1 -= learning_rate * da1
+        a0 -= learning_rate * da0
+
+        print("Coefficients (a0, a1) avec Gradient Descent :\n", a0, a1)
+    return da0, da1
+
+#def ComputeSimpleLinRegresCoefStochasticGradientDescentMatrix(x, y, learning_rate=0.01, max_iterations=1000, tolerance = 1e-6):
 
 def main():
 
@@ -62,10 +89,28 @@ def main():
     y = df["y"].to_numpy()
     
     # calculer les coefficients de la regression linéaire simple
-    a1, a0 = ComputeSimpleLinRegresCoefAnalytical(x, y)
+    a0, a1 = ComputeSimpleLinRegresCoefAnalytical(x, y)
 
     # Calculer les coefficients de la regression linéaire simple avec la méthode matricielle
-    a1, a0 = ComputeSimpleLinRegresCoefMatrix(x, y)
+    a0, a1 = ComputeSimpleLinRegresCoefMatrix(x, y)
+
+    # marker = "+" pour des points en +
+    # "ob" pour des points en o de couleur bleue
+    # plt.plot(x,y, "+b" ) pour des points en + de couleur bleue
+
+    #traçage du nuage de points
+    plt.plot(x,y, "ob" )
+
+    # a, b = np.polyfit(x, y, 1) calcule la pente et l'ordonnée à l'origine pour vous.
+    # plt.scatter(x, y) #nuage de point simple
+
+    plt.plot(x, a1*x + a0, color='red')
+
+    plt.ylabel('Regression linéaire')
+    plt.show()
+
+    a0, a1 = ComputeSimpleLinRegresCoefGradientDescent(x, y)
+    
     
 
 if __name__ == "__main__":
